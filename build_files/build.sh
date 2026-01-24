fc5b5502b5382b74cb83a35fbeadeb910e0f424d
tyranozomby <tyranozomby@gmail.com>
2026-01-24 18:03:49 +0100

    fix rechunker and add script to enable numpad before luks

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y htop
+#dnf5 install -y htop
+
+cp -ri /ctx/51numlock /usr/lib/dracut/modules.d
 
 # Use a COPR Example:
 #
